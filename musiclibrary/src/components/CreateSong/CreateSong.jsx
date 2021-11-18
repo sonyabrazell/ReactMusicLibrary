90a2e8bf3e8f5ad92847c332211cf8d13856cd48
sonya brazell <92110758+sonyabrazell@users.noreply.github.com>
2021-11-18 17:14:32 -0600

    Edit to CreateSong component. End of day save. Currently able to load page with few errors.

--- musiclibrary/src/components/CreateSong/CreateSong.jsx
+++ musiclibrary/src/components/CreateSong/CreateSong.jsx
@@ -20,33 +20,54 @@ class CreateSong extends Component {
 
     }
     handleSubmit = (event) => {
-        let response = axios.post('http://127.0.0.1:8000/music/')
         event.preventDefault();
+        axios.post('http://127.0.0.1:8000/music/')
+        .then(response =>{
         this.setState({
         library: response.data
-        }
-        )}
+        },
+        )})
+    }
 
-    render(){ 
-        return ( 
-            <form onSubmit={(event) => this.handleSubmit(event)}>
-            <div>
-                <label>Title: </label>
-                <input type= 'text' name='title' onChange={this.handleChange} value={this.state.library}></input>
-                <label>Artist: </label>
-                <input type= 'text' name='artist' onChange={this.handleChange} value={this.state.library}></input>
-                <label>Album: </label>
-                <input type= 'text' name='album' onChange={this.handleChange} value={this.state.library}></input>
-                <label>Genre: </label>
-                <input type= 'text' name='genre' onChange={this.handleChange} value={this.state.library}></input>
-                <label>Release Date: </label>
-                <input type= 'text' name='title' onChange={this.handleChange} value={this.state.library}></input>
-                <button onClick={()=>this.handleSubmit('')}>Add</button>
-            </div>
-            </form>
+render(){
+    return ( 
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+        <div>
+            <label>Title: </label>
+            <input type= 'text' 
+            name='title' 
+            onChange={this.handleChange} 
+            value={this.state.library}></input>
+
+            <label>Artist: </label>
+            <input type= 'text' 
+            name='artist' 
+            onChange={this.handleChange} 
+            value={this.state.library}></input>
+
+            <label>Album: </label>
+            <input type= 'text' 
+            name='album' onChange={this.handleChange} 
+            value={this.state.library}></input>
+
+            <label>Genre: </label>
+            <input type= 'text'
+                name='genre' 
+                onChange={this.handleChange} 
+                value={this.state.library}></input>
+
+            <label>Release Date: </label>
+            <input type= 'text' 
+            name='title' 
+            onChange={this.handleChange} 
+            value={this.state.library}></input>
+
+            <button type='submit'
+            onClick={()=>this.handleSubmit('')}>Add</button>
+        </div>
+        </form>
             
          );
-    }
-}
- 
+    
+    }}
 export default CreateSong;
